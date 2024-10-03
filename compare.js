bde6e741c00e6cfa840eba088f91c04da44d4987
g.sawada <sawada.gota@gmail.com>
2024-10-04 01:22:02 +0900

    compare.jsの追加

---
+++ compare.js
@@ -0,0 +1,14 @@
+// 関数宣言
+function hello1(name) {
+  console.log(`こんにちは${name}さん!`);
+}
+
+hello1("山田");
+
+
+// 関数式
+const hello2 = function(name) {
+  console.log(`こんにちは${name}さん!`);
+}
+
+hello2("田中");
