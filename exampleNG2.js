d0030831a6b5a2fdb7c18ecdd65a56d02e3f9298
g.sawada <sawada.gota@gmail.com>
2024-10-04 00:23:24 +0900

    exampleNG2の作成

--- exampleNG2.js
+++ exampleNG2.js
@@ -1,15 +1,15 @@
-// エラー例1
+// NG例2
 function main(){
   // システム日付から当日を取得
   const today = new Date();
 
-  const year = String(today.getFullYear());
-  const month = String(today.getMonth() + 1).padStart(2, '0');   // 月は0から始まるため+1
-  const day = String(today.getDate()).padStart(2, '0');
+  let year = String(today.getFullYear());
+  let month = String(today.getMonth() + 1).padStart(2, '0');   // 月は0から始まるため+1
+  let day = String(today.getDate()).padStart(2, '0');
   const formattedDate = `${year}/${month}/${day}`;
 
-  const year = today.getFullYear();
-  const month = today.getMonth() + 1;  // 月は0から始まるため+1
+  year = today.getFullYear();
+  month = today.getMonth() + 1;  // 月は0から始まるため+1
   const fiscalYear = month <= 3 ? `${(year - 1)}年度` : `${year}年度`;
 
   console.log(`今日は${formattedDate}です。`);
