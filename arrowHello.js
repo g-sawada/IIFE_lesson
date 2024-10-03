d3a611313c0449836eb900635ab3b391fd08b4e3
g.sawada <sawada.gota@gmail.com>
2024-10-04 07:53:42 +0900

    arrowHello.jsの追加

---
+++ arrowHello.js
@@ -0,0 +1,7 @@
+// arrowHello.js
+const hello2 = ((name) => {
+  console.log(`こんにちは、${name}さん！`);
+})
+
+// 実行
+hello2("山田");
--- funcExpression.js
+++ funcExpression.js
@@ -1,8 +1,5 @@
 // funcExpression.js
-function main(){ 
-  // システム日付から当日を取得
-  const today = new Date();
-  
+function main(){   
   // フォーマット済み年月日を取得する関数
   const getFormattedDate = function(date){
     const year = String(date.getFullYear());
@@ -17,6 +14,9 @@ function main(){
     const month = date.getMonth() + 1;  // 月は0から始まるため+1
     return month <= 3 ? `${(year - 1)}年度` : `${year}年度`;
   };
+
+  // システム日付から当日を取得
+  const today = new Date();
 
   // todayを与えてそれぞれの関数を実行
   const formattedDate = getFormattedDate(today);
